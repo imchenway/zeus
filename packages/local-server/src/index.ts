0fbb4dfa7573949afe5a17b972eaf6194393fa06
chenwei <imchenway@gmail.com>
2026-09-03 11:03:22 +0800

    style(release): 自动修复 v0.3.98 发布提交格式

--- packages/local-server/src/index.ts
+++ packages/local-server/src/index.ts
@@ -66,7 +66,7 @@ import {
   LongTermMemoryRepository,
   PluginRepository,
   ProjectionDatabaseRuntimeManager,
-    ProjectRepository,
+  ProjectRepository,
   ProjectRepositoryRegistrationRepository,
   ProjectSharedPathRepository,
   ProviderEventReceiptRepository,
